ed38742da3ce8c03ba10abb2b1885041b00465aa
leejinwoo <leejinwoo@leejinwooui-MacBookAir.local>
2025-07-20 19:56:20 +0900

    feat: README.md 추가

--- src/_common/prisma/prisma.module.ts
+++ src/_common/prisma/prisma.module.ts
@@ -1,11 +1,11 @@
-import { Global, Module } from '@nestjs/common';
-import { Logger } from 'winston';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-import { ConfigService } from '@nestjs/config';
-import { PrismaService } from './prisma.service';
-import { ClsService } from 'nestjs-cls';
+import { Global, Module } from '@nestjs/common'
+import { Logger } from 'winston'
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
+import { ConfigService } from '@nestjs/config'
+import { PrismaService } from './prisma.service'
+import { ClsService } from 'nestjs-cls'
 
-export const PRISMA_SERVICE = 'PrismaService';
+export const PRISMA_SERVICE = 'PrismaService'
 
 @Global()
 @Module({
